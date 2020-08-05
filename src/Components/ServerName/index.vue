a08c9063b37fd7dcb54ed222379667a8799da916
Igor Tuag~ <igortuag@gmail.com>
2020-08-05 10:32:14 -0300

    Implementation ServerName Component

---
+++ src/Components/ServerName/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="container__title">Servidor Vue</div>
+    <div class="container__icon">
+      <ExpandIcon size="28" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ExpandIcon from 'vue-material-design-icons/ChevronDown';
+
+export default {
+  components: {
+    ExpandIcon
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  grid-area: SN;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: var(--secondary);
+  padding: 0 11px 0 16px;
+  box-shadow: rgba(0, 0, 0, 0.9) 0 1px 0 0;
+  z-index: 2;
+
+  &__title {
+    color: var(--white);
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  &__icon {
+    color: var(--white);
+    cursor: pointer;
+  }
+}
+</style>
